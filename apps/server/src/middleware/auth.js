34838883b7c35540c7eb21a78751e858b43a9aa2
“Remi <tuu59529@temple.edu>
2026-03-31 16:02:16 -0400

    still at it

--- apps/server/src/middleware/auth.js
+++ apps/server/src/middleware/auth.js
@@ -1,6 +1,5 @@
 import { decode } from "next-auth/jwt";
 import User from "../models/User.js";
-import { json } from "express";
 
 export async function requireAuth(req, res, next) {
   try {
@@ -8,7 +7,9 @@ export async function requireAuth(req, res, next) {
       req.cookies["__Secure-authjs.session-token"] ||
       req.cookies["authjs.session-token"];
 
-    if (!token) return res.status(401).json({ error: "Unauthorizedz == " + JSON.stringify(req.cookies) });
+      console.error(req.cookies);
+
+    if (!token) return res.status(401).json({ error: "Unauthorized" });
 
     const saltStr = 'authjs.session-token';
 
@@ -22,7 +23,7 @@ export async function requireAuth(req, res, next) {
       salt: saltStr,
     });
 
-    if (!payload) return res.status(401).json({ error: "Unauthorize" });
+    if (!payload) return res.status(401).json({ error: "Unauthorized" });
 
     // get user id using email from payload
     const usr = await User.findOne({ email: payload.email });
